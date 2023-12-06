7af823c35f49f0935c060123c5072384a5f26579
dashpen <dashpenning@gmail.com>
2023-12-05 16:05:42 -0800

    added floor

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 const frameRate = 60
 
+const startingCameraHeight = 10
+
 let movingForward = false
 let movingBackward = false
 let movingLeft = false
@@ -24,12 +26,25 @@ let renderer = new THREE.WebGLRenderer({
 let camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 100) // perspective camera
 
 camera.position.x = 3
+camera.position.y = startingCameraHeight
+
 camera.lookAt(0,0,0)
 scene.add(camera)
+
+const floorGeo = new THREE.PlaneGeometry(1000, 1000)
+const floorMat = new THREE.MeshBasicMaterial({color: 0x808080})
+const floor = new THREE.Mesh(floorGeo, floorMat)
+
+floor.rotateX(-Math.PI/2)
+
+scene.add(floor)
 
 const boxGeo = new THREE.BoxGeometry(1, 1, 1)
 const boxMat = new THREE.MeshBasicMaterial({color: 0xff0000})
 const box = new THREE.Mesh(boxGeo, boxMat)
+
+box.position.y = 1
+
 scene.add(box)
 
 let controls = new PointerLockControls(camera, document.body)
@@ -92,7 +107,7 @@ render()
 
 // logic
 
-const runSpeed = 0.2
+const runSpeed = 0.1
 const accel = 0.2
 
 function move(){
